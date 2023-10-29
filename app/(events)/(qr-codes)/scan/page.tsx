bd4d6699558f426ab23799acb438dba5640f8006
10lite <rcpaderna@up.edu.ph>
2023-10-29 20:52:54 +0800

    feat Scan QR Code page init

--- app/(events)/(qr-codes)/scan/page.tsx
+++ app/(events)/(qr-codes)/scan/page.tsx
@@ -1,9 +1,24 @@
 import PageHeader from "@/components/(nav)/PageHeader";
+import { CameraIcon } from "@radix-ui/react-icons";
 
 export default function Scan() {
     return (
-        <main className="min-h-screen flex flex-col">
+        <main className="min-h-screen flex flex-col items-center bg-var-primary-30 bg-opacity-80">
             <PageHeader />
+            <section className="flex flex-col gap-4 items-center md:max-w-[50%] lg:max-w-[30%]">
+                <header className="text-center text-white">
+                    <h2 className="text-xl">Logging Attendance for</h2>
+                    <h1 className="font-bold text-3xl">Event Title</h1>
+                </header>
+                <figure className="w-full  text-var-primary-30 pb-4 bg-white">
+                    <CameraIcon className="w-56 h-56 mx-auto opacity-10" />
+                </figure>
+                <section>
+                    <h3 className="text-xl font-medium text-center text-white">Point the QR code in the center.</h3>
+                    <p className="text-justify pt-4 text-white">Scanning a QR code results in the storage of an attendee&apos;s information in the event&apos;s database. Remember to ask for their QR code!</p>
+                    <p className="text-var-primary-10 pt-4 text-justify">Disclaimer: Only scan a QR code once.</p>
+                </section>
+            </section>
         </main>
     )
 }
