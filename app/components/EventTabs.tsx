529446c6fd01b1e85c31c24e2e023082e88fad08
10lite <rcpaderna@up.edu.ph>
2023-10-31 22:04:59 +0800

    fix Landing Page for Admin initialization

--- app/components/EventTabs.tsx
+++ app/components/EventTabs.tsx
@@ -1,16 +1,37 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function EventTabs() {
+    const user = 'admin'
+
+    const tabData = user === 'admin'
+        ? [
+            { value: "pending", label: "Pending", colorClass: "text-yellow-500" },
+            { value: "completed", label: "Completed", colorClass: "text-var-secondary-20" },
+            { value: "closed", label: "Closed", colorClass: "text-var-primary-30" },
+        ] : [
+            { value: "upcoming", label: "Upcoming", colorClass: "text-var-primary-30, data-[state=active]:text-var-secondary-20" },
+            { value: "ongoing", label: "Ongoing", colorClass: "text-var-primary-30, data-[state=active]:text-var-secondary-20" },
+            { value: "past", label: "Past", colorClass: "text-var-primary-30, data-[state=active]:text-var-secondary-20" },
+        ];
+
     return (
-        <Tabs defaultValue="upcoming" className="w-ful flex flex-col">
+        <Tabs defaultValue={user === 'admin' ? "pending" : "upcoming"} className="w-full flex flex-col">
             <TabsList className="mx-auto">
-                <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
-                <TabsTrigger value="ongoing">Ongoing</TabsTrigger>
-                <TabsTrigger value="past">Past</TabsTrigger>
-            </TabsList> 
-            <TabsContent value="upcoming">Upcoming Events</TabsContent>
-            <TabsContent value="ongoing">Ongoing Events</TabsContent>
-            <TabsContent value="past">Past Events</TabsContent>
+                {tabData.map((tab, index) => (
+                    <TabsTrigger
+                        key={index}
+                        value={tab.value}
+                        className={tab.colorClass}
+                    >
+                        {tab.label}
+                    </TabsTrigger>
+                ))}
+            </TabsList>
+            {tabData.map((tab) => (
+                <TabsContent key={tab.value} value={tab.value}>
+                    {tab.label} {user === 'admin' ? "Requests" : "Events"}
+                </TabsContent>
+            ))}
         </Tabs>
     )
 }
