bcce8cffa03e4f1d8c0fe01cfd53c884aa5079ac
10lite <rcpaderna@up.edu.ph>
2023-11-01 22:27:50 +0800

    feat Calendar Page init

--- app/(events)/calendar/page.tsx
+++ app/(events)/calendar/page.tsx
@@ -1,10 +1,12 @@
 import PageHeader from "@/components/(nav)/PageHeader";
 import ButtonMenu from "@/components/(nav)/ButtonMenu";
+import Calendar from "./components/Calendar";
 
-export default function Calendar() {
+export default function calendar() {
     return (
         <main className="min-h-screen flex flex-col items-center">
             <PageHeader />
+            <Calendar />
             <ButtonMenu />
         </main>
     )
