60df3da368ce108fac9aa3f64e36e52cbcbcb412
10lite <rcpaderna@up.edu.ph>
2023-11-02 21:26:20 +0800

    fix Accessibility fixes

--- components/(buttons)/AccountButton.tsx
+++ components/(buttons)/AccountButton.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 
 export default function AccountButton() {
     return (
-        <Link href="/account" className="absolute right-0 md:right-[20%] lg:right-[30%] mr-6">
-            <Button variant="ghost" className="p-0 text-white hover:bg-red-500 hover:text-red-200">
+        <Link href="/account" className="absolute right-0 md:right-[20%] lg:right-[30%] mr-6" aria-label="Account">
+            <Button id="account" variant="ghost" className="p-0 text-white hover:bg-red-500 hover:text-red-200" aria-label="Account">
                 <PersonIcon className="h-6 w-6"/>
             </Button>
         </Link>
