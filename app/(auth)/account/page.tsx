8769e6746c26cfa8d5562e35bc566bd0097e8cb2
10lite <rcpaderna@up.edu.ph>
2023-11-02 21:59:55 +0800

    feat Account Info init

--- app/(auth)/account/page.tsx
+++ app/(auth)/account/page.tsx
@@ -1,10 +1,14 @@
+// Components
+import PageHeader from "@/components/(nav)/PageHeader";
+import AccountInfo from "./components/AccountInfo";
 import AppInfoAccordion from "./components/AppInfoAccordion"
 import CallToActionButton from "@/components/(buttons)/CallToActionButton";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { AccountInfoTabs } from "./components/AccountInfoTabs"
+
+// shadCN Components
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
-import PageHeader from "@/components/(nav)/PageHeader";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Separator } from "@/components/ui/separator";
 
 export default function Account() {
     return (
@@ -21,20 +25,20 @@ export default function Account() {
                     <p>2021-XXXXX</p>
                 </>
             </section>
-            <section className="w-[90%] md:w-[50%] lg:w-[40%] flex flex-col items-center">
-                <span className="flex items-center space-x-2 mb-2">
+            <section className="w-[90%] md:w-[50%] lg:w-[40%] flex flex-col items-center px-6">
+                {/* <span className="flex items-center space-x-2 mb-2">
                     <Switch id="toggle-account-info" />
                     <Label htmlFor="toggle-account-info">View Account Info</Label>
-                </span>
-                {/* <AccountInfoTabs /> */}
+                </span> */}
+                <AccountInfo />
             </section>
+            <Separator className="my-4 w-[100%] md:w-[50%] lg:w-[40%]" />
             <section className="w-[90%] md:w-[50%] lg:w-[40%] px-6">
                 <AppInfoAccordion />
             </section>
-            <nav className="relative mt-12">
+            {/* <nav className="relative mt-12">
                 <CallToActionButton action="Sign Out" />
-            </nav>
-            {/* <ButtonMenu /> */}
+            </nav> */}
         </main>
     )
 }
