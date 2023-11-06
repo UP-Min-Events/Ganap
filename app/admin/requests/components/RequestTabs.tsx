c7b5360a4ff3a8ee63484eb29a985e2bf4f58897
10lite <rcpaderna@up.edu.ph>
2023-11-06 17:14:09 +0800

    fix Separate event tabs from request tabs

---
+++ app/admin/requests/components/RequestTabs.tsx
@@ -0,0 +1,30 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+export default function RequestTabs() {
+    const tabData = [
+        { value: "pending", label: "Pending", colorClass: "data-[state=active]:bg-red-400 data-[state=active]:text-white" },
+        { value: "completed", label: "Completed", colorClass: "data-[state=active]:bg-red-500 data-[state=active]:text-neutral-100" },
+        { value: "closed", label: "Closed", colorClass: "data-[state=active]:bg-red-600 data-[state=active]:text-neutral-100" },
+    ];
+
+    return (
+        <Tabs defaultValue={"pending"} className="w-full flex flex-col">
+            <TabsList className="mx-auto">
+                {tabData.map((tab, index) => (
+                    <TabsTrigger
+                        key={index}
+                        value={tab.value}
+                        className={tab.colorClass}
+                    >
+                        {tab.label}
+                    </TabsTrigger>
+                ))}
+            </TabsList>
+            {tabData.map((tab) => (
+                <TabsContent key={tab.value} value={tab.value}>
+                    {tab.label} Requests
+                </TabsContent>
+            ))}
+        </Tabs>
+    )
+}
--- app/admin/requests/page.tsx
+++ app/admin/requests/page.tsx
@@ -1,6 +1,6 @@
 // Components
-import EventCard from "@/app/components/EventCard"
-import EventTabs from "@/app/components/EventTabs"
+import RequestTabs from "./components/RequestTabs"
+// import RequestCard from "./components/RequestCard"
 import ButtonMenu from "@/components/(nav)/ButtonMenu"
 import PageHeader from "@/components/(nav)/PageHeader"
 
@@ -11,12 +11,12 @@ export default function Requests() {
       <PageHeader />
       <section className="container flex flex-col px-8 lg:max-w-[40%]">
         <header className="text-2xl font-bold mb-2">
-          <EventTabs /> 
+          <RequestTabs /> 
         </header>
         <>
-          <EventCard />
-          <EventCard />
-          <EventCard />
+          {/* <RequestCard />
+          <RequestCard />
+          <RequestCard /> */}
         </>
       </section>
       <ButtonMenu />
