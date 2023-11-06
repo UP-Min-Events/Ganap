5196f0480927bf3c92d40f4f9dc4fdbd844e03c1
10lite <rcpaderna@up.edu.ph>
2023-11-06 22:50:46 +0800

    fix Page Header font size consistency

--- app/(auth)/account/edit/page.tsx
+++ app/(auth)/account/edit/page.tsx
@@ -16,7 +16,7 @@ export default function Edit({ searchParams
     <main className="min-h-screen flex flex-col items-center">
       <header className="px-6 w-full flex justify-center relative bg-red-500 text-white py-4 overflow-hidden">
         <BackButton />
-        <h1 className="text-[1.5rem] font-semibold text-center">Edit Account Info</h1>
+        <h1 className="text-[1.75rem] font-semibold text-center">Edit Account Info</h1>
       </header>
       <section className="flex flex-col items-center py-6">
         <h2 className="text-2xl font-bold">{searchParams.info}</h2>
--- app/(events)/event/page.tsx
+++ app/(events)/event/page.tsx
@@ -14,7 +14,7 @@ export default function Event() {
         <main>
             <header className="px-6 w-full flex justify-center relative bg-red-500 text-white py-4 overflow-hidden">
                 <BackButton />
-                <h1 className="text-[1.5rem] md:text-[1.75rem] lg:text-4xl font-semibold text-center">Event Title</h1>
+                <h1 className="text-[1.75rem] font-semibold text-center">Event Title</h1>
             </header>
             <Separator className="mb-4" />
             <section className="container flex flex-col gap-4 md:max-w-[60%] lg:max-w-[40%]">
