e501cc35b076b638112d617459b8efbe6fbc0350
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2023-11-08 00:02:21 +0800

    feat (event_comment api): added get comments functionality

--- app/api/(event_comments)/comment/[eventId]/route.js
+++ app/api/(event_comments)/comment/[eventId]/route.js
@@ -1,7 +1,13 @@
-import { postComment } from "../../../_utils/event_comments";
+import { postComment, getComments } from "../../../_utils/event_comments";
 
 export const POST = async (request, { params }) => {
     const { eventId } = await params;
     const response = await postComment(await request.json(), eventId, "EventComments");
+    return response;
+}
+
+export const GET = async (_, { params }) => {
+    const { eventId } = await params;
+    const response = await getComments(eventId, "EventComments");
     return response;
 }
