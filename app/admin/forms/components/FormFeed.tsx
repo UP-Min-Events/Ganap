95ce1747f6d0aac90266694f48530bf4ae52fdc3
10lite <rcpaderna@up.edu.ph>
2023-11-10 17:13:00 +0800

    fix Admin Forms Page UI Fix

--- app/admin/forms/components/FormFeed.tsx
+++ app/admin/forms/components/FormFeed.tsx
@@ -1,13 +1,16 @@
 // Components
 import DownloadableFormCard from "./DownloadableFormCard";
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 export default function FormFeed() {
   return (
-    <section className="container grid gap-4 grid-cols-2 place-items-center lg:max-w-[50%] mt-4">
-      <DownloadableFormCard />
-      <DownloadableFormCard />
-      <DownloadableFormCard />
-      <DownloadableFormCard />
-    </section>
+    <ScrollArea className="max-h-[calc(100vh-12rem)] overflow-scroll w-full no-scrollbar">
+      <section className="container flex flex-col gap-4 sm:grid sm:grid-cols-2 place-items-center md:max-w-[75%] lg:max-w-[75%] xl:max-w-[50%] mt-4">
+          <DownloadableFormCard />
+          <DownloadableFormCard />
+          <DownloadableFormCard />
+          <DownloadableFormCard />
+      </section>
+    </ScrollArea>
   )
 }
