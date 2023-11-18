e9f2f589524bc39b63f40019ac1310ae50c2a3df
10lite <rcpaderna@up.edu.ph>
2023-11-18 09:55:45 +0800

    fix Calendar cells fix

--- app/(events)/calendar/components/Calendar.tsx
+++ app/(events)/calendar/components/Calendar.tsx
@@ -1,26 +1,45 @@
 // Components
 import CalendarHeader from "./CalendarHeader";
 
+interface Day {
+  full: string;
+  short: string;
+}
+
 export default function Calendar() {
-  const weekdays = [
+  const weekdays: Day[] = [
     { full: "Sunday", short: "Sun" },
     { full: "Monday", short: "Mon" },
     { full: "Tuesday", short: "Tue" },
     { full: "Wednesday", short: "Wed" },
     { full: "Thursday", short: "Thu" },
     { full: "Friday", short: "Fri" },
-    { full: "Saturday", short: "Sat" }
+    { full: "Saturday", short: "Sat" },
   ];
 
-  const renderTableCell = (day: { full: string, short: string }) => (
-    <td key={day.full} className="group border h-6 lg:h-40 overflow-auto transition duration-500 ease hover:bg-gray-300">
+  const renderTableCell = (day: number, weekday: Day) => (
+    <td key={day} className="group border h-6 lg:h-40 overflow-auto transition duration-500 ease hover:bg-gray-300">
       <div className="flex flex-col h-full w-full overflow-hidden">
         <div className="w-full flex justify-center h-4 lg:mb-1">
-          <span className="text-gray-500 text-xs lg:text-base">{day.full}</span>
+          <span className="text-gray-500 text-xs lg:text-base">{day}</span>
         </div>
         <div className="bottom flex-grow h-30 py-1 w-full cursor-pointer" />
       </div>
     </td>
+  );
+
+  const renderTableRow = (startDay: number) => (
+    <tr key={startDay} className="text-center h-[6rem]">
+      {Array.from({ length: 7 }, (_, index) => {
+        const day = startDay + index;
+        if (day <= 31) {
+          const weekday = weekdays[(day - 1) % 7];
+          return renderTableCell(day, weekday);
+        } else {
+          return null; // To render an empty cell for days beyond the current month
+        }
+      })}
+    </tr>
   );
 
   return (
@@ -39,11 +58,7 @@ export default function Calendar() {
             </tr>
           </thead>
           <tbody>
-            {[1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31].map((day) => (
-              <tr key={day} className="text-center h-[6rem]">
-                {weekdays.map((weekday) => renderTableCell(weekday))}
-              </tr>
-            ))}
+            {[1, 8, 15, 22, 29].map((startDay) => renderTableRow(startDay))}
           </tbody>
         </table>
       </span>
