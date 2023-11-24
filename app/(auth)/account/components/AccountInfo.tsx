c33c2c8f49445e271da65c6461c15b755c7100d1
Nhyl Bryle Ibañez <nbryleibanez@gmail.com>
2023-11-24 12:10:33 +0800

    fetch user data

--- app/(auth)/account/components/AccountInfo.tsx
+++ app/(auth)/account/components/AccountInfo.tsx
@@ -4,85 +4,94 @@ import { ChevronRightIcon } from "@radix-ui/react-icons"
 
 // shadCN Components
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { cookies } from "next/headers"
 
-export default function AccountInfo() {
-  return (
-    <>
-      <section className="flex flex-col items-center pb-4">
-        <Avatar className="w-32 h-32 mb-4">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>HI</AvatarFallback>
-        </Avatar>
+export default async function AccountInfo() {
+
+    const origin = window.location.origin
+    const cookieStore = cookies()
+    const sub = cookieStore.get("sub") 
+
+    const response = await fetch(`${origin}/api/users/${sub?.value}`)
+    const data = await response.json()
+
+    return (
         <>
-            <h1 className="text-2xl font-bold">Nhyl Bryle</h1>
-            <p>emailaddress@up.edu.ph</p>
-            <p>2021-XXXXX</p>
+            <section className="flex flex-col items-center pb-4">
+                <Avatar className="w-32 h-32 mb-4">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>HI</AvatarFallback>
+                </Avatar>
+                <>
+                    <h1 className="text-2xl font-bold">Nhyl Bryle</h1>
+                    <p>emailaddress@up.edu.ph</p>
+                    <p>2021-XXXXX</p>
+                </>
+            </section>
+            <section className="w-[90%] md:w-[50%] lg:w-[40%] flex flex-col items-center px-6">
+                <section className="xl:max-w-[75%] flex flex-col items-center w-full shadow-[0_2px_2px_0_rgba(0,0,0,0.2)] rounded-lg">
+                    <Link
+                        href={{
+                            pathname: "/account/edit",
+                            query: {
+                                info: "name",
+                                name: "Nhyl Bryle",
+                            }
+                        }}
+                        className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 rounded-t-lg w-full p-4 text-sm font-medium"
+                    >
+                        Name <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
+                    </Link>
+                    <Link
+                        href={{
+                            pathname: "/account/edit",
+                            query: {
+                                info: "studentNumber",
+                                studentNumber: "2021-XXXXX",
+                            }
+                        }}
+                        className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 w-full p-4 text-sm font-medium"
+                    >
+                        Student Number <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
+                    </Link>
+                    <Link
+                        href={{
+                            pathname: "/account/edit",
+                            query: {
+                                info: "yearLevel",
+                                yearLevel: "2021-XXXXX",
+                            }
+                        }}
+                        className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 w-full p-4 text-sm font-medium"
+                    >
+                        Year Level <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
+                    </Link>
+                    <Link
+                        href={{
+                            pathname: "/account/edit",
+                            query: {
+                                info: "degreeProgram",
+                                degreeProgram: "BS Computer Science",
+                            }
+                        }}
+                        className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 w-full p-4 text-sm font-medium"
+                    >
+                        Degree Program <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
+                    </Link>
+                    <Link
+                        href={{
+                            pathname: "/account/edit",
+                            query: {
+                                info: "organizations",
+                                organizations: "Organization 1, Organization 2, Organization 3",
+                            }
+                        }}
+                        className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 w-full p-4 text-sm font-medium"
+                    >
+                        Organizations <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
+                    </Link>
+                </section>
+            </section>
         </>
-      </section>
-      <section className="w-[90%] md:w-[50%] lg:w-[40%] flex flex-col items-center px-6">
-        <section className="xl:max-w-[75%] flex flex-col items-center w-full shadow-[0_2px_2px_0_rgba(0,0,0,0.2)] rounded-lg">
-          <Link 
-            href={{
-              pathname: "/account/edit",
-              query: {
-                info: "name",
-                name: "Nhyl Bryle", 
-              }
-            }}
-            className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 rounded-t-lg w-full p-4 text-sm font-medium"
-          >
-            Name <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
-          </Link>
-          <Link 
-            href={{
-              pathname: "/account/edit",
-              query: {
-                info: "studentNumber",
-                studentNumber: "2021-XXXXX", 
-              }
-            }}
-            className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 w-full p-4 text-sm font-medium"
-          >
-            Student Number <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
-          </Link>
-          <Link 
-            href={{
-              pathname: "/account/edit",
-              query: {
-                info: "yearLevel",
-                yearLevel: "2021-XXXXX", 
-              }
-            }}
-            className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 w-full p-4 text-sm font-medium"
-          > 
-            Year Level <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
-          </Link>
-          <Link 
-            href={{
-              pathname: "/account/edit",
-              query: {
-                info: "degreeProgram",
-                degreeProgram: "BS Computer Science", 
-              }
-            }}
-            className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 w-full p-4 text-sm font-medium"
-          >
-            Degree Program <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
-          </Link>
-          <Link 
-            href={{
-              pathname: "/account/edit",
-              query: {
-                info: "organizations",
-                organizations: "Organization 1, Organization 2, Organization 3", 
-              }
-            }}
-            className="flex group justify-between items-center border border-b-0 border-neutral-300 hover:bg-neutral-100 w-full p-4 text-sm font-medium"
-          >
-            Organizations <ChevronRightIcon className="w-[1.25rem] h-[1.25rem] text-red-500" />
-          </Link>
-        </section>
-      </section>
-    </>
-  )
+    )
 }
