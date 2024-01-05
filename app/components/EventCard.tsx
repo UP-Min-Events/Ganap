d026851a01a91c381e8daf40361f8226f57b45a9
ASPactores <anakin.pactores@gmail.com>
2024-01-05 17:36:12 +0800

    fix: formatting fix in EventCard.tsx

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -30,17 +30,21 @@ export default function EventCard({ event }: { event: EventDetails }) {
                             by {event.organizer}
                         </CardDescription>
                     </CardHeader>
-                    <CardContent className='flex justify-between align-bottom pl-2 pr-0 py-0'>
-                        <section className='text-sm leading-3 space-y-1'>
+                    <CardContent className="flex justify-between align-bottom pl-2 pr-0 py-0">
+                        <section className="text-sm leading-3 space-y-1">
                             <h3 className="font-semibold text-red-500 flex items-center gap-1 [&>svg]:w-[0.75rem] [&>svg]:h-[0.75rem]">
-                                <Calendar /> {moment(event.start_date).format('LL, h:mm A')}
+                                <Calendar />{' '}
+                                {moment(event.start_date).format('LL, h:mm A')}
                             </h3>
                             <h3 className="font-semibold flex items-center gap-1 [&>svg]:w-[0.75rem] [&>svg]:h-[0.75rem]">
                                 <MapPin /> {event.venue}
                             </h3>
                         </section>
-                        <Button size="icon_s" className="bg-red-100 group-hover:bg-red-200 text-red-500 group-hover:text-red-600 [&>svg]:w-[1rem] [&>svg]:h-[1rem]">
-                            <ArrowUpRight /> 
+                        <Button
+                            size="icon_s"
+                            className="bg-red-100 group-hover:bg-red-200 text-red-500 group-hover:text-red-600 [&>svg]:w-[1rem] [&>svg]:h-[1rem]"
+                        >
+                            <ArrowUpRight />
                         </Button>
                     </CardContent>
                 </section>
