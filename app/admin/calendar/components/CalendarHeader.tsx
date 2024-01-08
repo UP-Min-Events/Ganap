b9f126b75afbce0854661a3a53c3551fe6578785
Nhyl Bryle Ibañez <nbryleibanez@gmail.com>
2024-01-08 22:08:42 +0800

    calendar feature

--- app/admin/calendar/components/CalendarHeader.tsx
+++ app/admin/calendar/components/CalendarHeader.tsx
@@ -2,15 +2,35 @@
 import { Button } from '@/components/ui/button';
 import { ChevronRightIcon, ChevronLeftIcon } from '@radix-ui/react-icons';
 
-export default function CalendarHeader() {
+interface Props {
+    month: string;
+    nextMonth: () => void;
+    previousMonth: () => void;
+}
+
+export default function CalendarHeader({
+    month,
+    nextMonth,
+    previousMonth,
+}: Props) {
     return (
         <header className="header mx-auto flex items-center justify-between border-b p-4 lg:max-w-[40%]">
-            <span className="text-lg font-bold">2023 November</span>
+            <span className="text-lg font-bold">{month}</span>
             <nav className="flex gap-1">
-                <Button variant="outline" size="icon" className="p-1">
+                <Button
+                    onClick={previousMonth}
+                    variant="outline"
+                    size="icon"
+                    className="p-1"
+                >
                     <ChevronLeftIcon className="h-4 w-4" />
                 </Button>
-                <Button variant="outline" size="icon" className="p-1">
+                <Button
+                    onClick={nextMonth}
+                    variant="outline"
+                    size="icon"
+                    className="p-1"
+                >
                     <ChevronRightIcon className="h-4 w-4" />
                 </Button>
             </nav>
--- app/admin/calendar/components/CalendarTable.tsx
+++ app/admin/calendar/components/CalendarTable.tsx
@@ -1,12 +1,57 @@
-// Components
+'use client';
+
 import CalendarHeader from './CalendarHeader';
+import { useState } from 'react';
+import Link from 'next/link';
+import {
+    add,
+    eachDayOfInterval,
+    endOfMonth,
+    format,
+    getDay,
+    isEqual,
+    isSameDay,
+    isSameMonth,
+    isToday,
+    parse,
+    parseISO,
+    startOfToday,
+} from 'date-fns';
 
 interface Day {
     full: string;
     short: string;
 }
 
-export default function CalendarTable() {
+interface Events {
+    Items: EventDetails[];
+    LastEvaluatedKey: LastEvaluatedKeyProp;
+}
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ');
+}
+
+export default function CalendarTable({ events }: { events: Events }) {
+    let today = startOfToday();
+    let [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'));
+    let firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date());
+
+    let days = eachDayOfInterval({
+        start: firstDayCurrentMonth,
+        end: endOfMonth(firstDayCurrentMonth),
+    });
+
+    function previousMonth() {
+        let firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 });
+        setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
+    }
+
+    function nextMonth() {
+        let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
+        setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
+    }
+
     const weekdays: Day[] = [
         { full: 'Sunday', short: 'Sun' },
         { full: 'Monday', short: 'Mon' },
@@ -50,7 +95,11 @@ export default function CalendarTable() {
     return (
         <section className="mb-24 w-full">
             <span className="wrapper w-full rounded bg-white shadow">
-                <CalendarHeader />
+                <CalendarHeader
+                    month={format(firstDayCurrentMonth, 'MMMM yyyy')}
+                    nextMonth={nextMonth}
+                    previousMonth={previousMonth}
+                />
                 <table className="w-full table-fixed">
                     <thead>
                         <tr>
@@ -69,13 +118,88 @@ export default function CalendarTable() {
                             ))}
                         </tr>
                     </thead>
-                    <tbody>
+                    {/* <tbody>
                         {[1, 8, 15, 22, 29].map((startDay) =>
                             renderTableRow(startDay),
                         )}
-                    </tbody>
+                    </tbody> */}
                 </table>
+                <div className="grid grid-cols-7">
+                    {days.map((day, dayIdx) => (
+                        <div
+                            key={day.toString()}
+                            className={classNames(
+                                (dayIdx === 0 &&
+                                    colStartClasses[getDay(day)]) ||
+                                    '',
+                                'ease group h-20 overflow-auto border transition duration-200 hover:bg-gray-300 lg:h-40',
+                            )}
+                        >
+                            <div className="flex h-full w-full flex-col overflow-hidden">
+                                <div className="flex h-4 w-full justify-center lg:mb-1">
+                                    <span
+                                        className={classNames(
+                                            isToday(day)
+                                                ? 'font-bold text-gray-900'
+                                                : '',
+                                            !isToday(day) &&
+                                                isSameMonth(
+                                                    day,
+                                                    firstDayCurrentMonth,
+                                                )
+                                                ? 'text-gray-900'
+                                                : '',
+                                            !isToday(day) &&
+                                                !isSameMonth(
+                                                    day,
+                                                    firstDayCurrentMonth,
+                                                )
+                                                ? 'text-gray-400'
+                                                : '',
+                                        )}
+                                    >
+                                        <time
+                                            dateTime={format(day, 'yyyy-MM-dd')}
+                                        >
+                                            {format(day, 'd')}
+                                        </time>
+                                    </span>
+                                </div>
+                                <div>
+                                    {events.Items.map((event) => (
+                                        <Link
+                                            href={`/event/${event.event_id}`}
+                                            key={event.event_id}
+                                        >
+                                            {isSameDay(
+                                                parseISO(
+                                                    event.start_date ?? '',
+                                                ),
+                                                day,
+                                            ) && (
+                                                <div>
+                                                    <p>{event.event_name}</p>
+                                                </div>
+                                            )}
+                                        </Link>
+                                    ))}
+                                </div>
+                                <div className="bottom h-30 w-full flex-grow cursor-pointer py-1" />
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </span>
         </section>
     );
 }
+
+let colStartClasses = [
+    '',
+    'col-start-2',
+    'col-start-3',
+    'col-start-4',
+    'col-start-5',
+    'col-start-6',
+    'col-start-7',
+];
